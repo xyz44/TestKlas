c13dd6e82d777a2d09bc60343c9b97225fa889ea
bparr <xyz44@onet.com.pl>
2019-08-28 21:57:30 +0200

    Dodano pole stanKonta

--- src/KlasaA.java
+++ src/KlasaA.java
@@ -6,6 +6,7 @@ import java.io.Serializable;
 public class KlasaA implements Serializable {
     private int d;
     private String napis;
+	private Double stanKonta;
 
     public KlasaA() {
         System.out.println("Konstruktor KlasyA");
